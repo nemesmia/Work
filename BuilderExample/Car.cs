4ba4f59889df72c1684055f9b934a94d1927f33f
Mihaiela Nemes <NemesM@haufe.com>
2017-07-27 14:09:30 +0200

    Add new project with car

---
+++ BuilderExample/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderExample
+{
+	public class Car
+	{
+		public string Name { get; set; }
+		public  int Speed { get; set; }
+		public  string Color { get; set; }
+
+		public void Drive()
+		{
+			Console.WriteLine($"Driving {Name} with speed {Speed} and color {Color}");
+		}
+
+	}
+}
---
+++ BuilderExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderExample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Car v = new Car();
+			v.Name = "Ford";
+			v.Speed = 100;
+			v.Color = "Grün";
+
+
+
+			v.Drive();
+
+			Console.ReadLine();
+		}
+	}
+}
